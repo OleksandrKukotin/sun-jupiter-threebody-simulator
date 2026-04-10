942b690d9a86b12c72bc3990c99c2e5b1b7fd281
OleksandrKukotin <oleksandr.kukotin@gmail.com>
2026-04-10 10:23:36 +0300

    Configured Gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,25 @@
 plugins {
     id("java")
+    id("org.springframework.boot") version "3.4.4"
+    id("io.spring.dependency-management") version "1.1.7"
 }
 
 group = "org.github.oleksandrkukotin"
 version = "1.0-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
+}
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:6.0.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.apache.commons:commons-math3:3.6.1")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 tasks.test {
